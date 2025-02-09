63ac10005d93559b9c4b439c7ea9e61982bafd13
Gourav Bhardwaj <gourav_bhardwaj@sfu.ca>
2025-02-09 00:36:37 -0800

    fixed an indexing error

--- C++ Basics/arrays.cpp
+++ C++ Basics/arrays.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-    int arr[6];
+    int arr[7];
     for (int i = 0; i < 5; i++)
     {
         arr[i] = i;
         printf("%d\n", arr[i]);
     }
     // the next statement prints the garbage value of the 6th element of the array, does not give an error
-    printf("%d\n", arr[6]);
+    printf("%d\n", arr[5]);
     // the next statement prints a garbage value, also does not give an error
     printf("%d\n", arr[10]);
     return 0;
