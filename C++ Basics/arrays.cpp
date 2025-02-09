a64678de1de32e42cea63ada7b76fe5e4547cc5f
Gourav Bhardwaj <gourav_bhardwaj@sfu.ca>
2025-02-09 00:44:43 -0800

    changes

--- C++ Basics/arrays.cpp
+++ C++ Basics/arrays.cpp
@@ -5,14 +5,29 @@ using namespace std;
 int main()
 {
     int arr[7];
+
+    // this is before initializating the array, prints garbage values
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     cout << arr[i] << endl;
+    // }
+
+    // initializing and printing first 5 elements of the array
     for (int i = 0; i < 5; i++)
     {
         arr[i] = i;
-        printf("%d\n", arr[i]);
     }
+
+    // print
+    for (int x : arr)
+    {
+        cout << x << endl;
+    }
+
     // the next statement prints the garbage value of the 6th element of the array, does not give an error
     printf("%d\n", arr[5]);
-    // the next statement prints a garbage value, also does not give an error
+
+    // the next statement prints a garbage value, i > size of array,also does not give an error
     printf("%d\n", arr[10]);
     return 0;
 }
