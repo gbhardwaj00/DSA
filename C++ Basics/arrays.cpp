15b8959620ddf66f4238f0f718daf82bf5b23a48
Gourav Bhardwaj <gourav_bhardwaj@sfu.ca>
2025-02-09 00:34:06 -0800

    array size related error-checking

--- C++ Basics/arrays.cpp
+++ C++ Basics/arrays.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main()
 {
+    int arr[6];
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i] = i;
+        printf("%d\n", arr[i]);
+    }
+    // the next statement prints the garbage value of the 6th element of the array, does not give an error
+    printf("%d\n", arr[6]);
+    // the next statement prints a garbage value, also does not give an error
+    printf("%d\n", arr[10]);
     return 0;
 }
