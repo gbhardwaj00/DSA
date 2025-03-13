850d5da7b248f29bdca180d165f126e476a6952e
Gourav Bhardwaj <gourav_bhardwaj@sfu.ca>
2025-03-13 16:52:23 -0700

    Completed recursion and solved quiz questions

--- recursion/quizQuestions.cpp
+++ recursion/quizQuestions.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// f(p, p)
-int f(int &x, int c)
-{
-    c = c - 1;
-    if (c == 0)
-        return 1;
-    x = x + 1;
-    return f(x, c) * x;
-}
-
 int main()
 {
     cout << "You should 'REALLY' do these by yourself " << endl;
@@ -18,7 +8,5 @@ int main()
     cout << "But if you feel stuck after trying them for a while,"
             "feel free to ping me at my IG - @goouuraav or"
             " e - mail at gouravbhardwaj3007@gmail.com "
-         << endl;
-    int a = 5;
-    cout << f(a, 5);
+<< endl;
 }
