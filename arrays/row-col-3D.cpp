256c818c64f2e1b86cc9e5032c2145857a8e2bad
Gourav Bhardwaj <gourav_bhardwaj@sfu.ca>
2025-03-17 15:30:09 -0700

    fixes

--- arrays/row-col-3D.cpp
+++ arrays/row-col-3D.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 int main()
 {
-    // sizes: l * m * n
+    // Pointer arithmatic takes care of multiplying by the sizeof automatically, compiler automatically scales based on the size of the data type
+
+    // Dimensions: l * m * n
     int A[3][4][4];
 
     // row major, formula created from left to right
     // Addr(A[i][j][k]) = BA + [i * m * n + j * n + k]
-    // BA + [i * 4 * 4 + j * 4 + k] * sizeOfInt
+    // BA + [i * 4 * 4 + j * 4 + k]
 
     // col major, formula created from right to left
-    //  Addr(A[i][j][k]) = BA + [k * l * m + j * l + i] * sizeOfInt
-    // BA + [k * 3 * 4 + j * 3 + i1] * sizeOfInt
+    //  Addr(A[i][j][k]) = BA + [k * l * m + j * l + i]
+    // BA + [k * 3 * 4 + j * 3 + i1]
 
     // setting values
     A[1][2][3] = 3;
 
     // testing row major as C++ uses that for default
-    cout << *(A + (1 * 3 * 4 + 2 * 3 + 3) * sizeof(int));
+    // (int *) flattens the 3D array into 1D array of int values
+    // * is a dereference operator
+    cout << *((int *)A + (1 * 4 * 4 + 2 * 4 + 3)) << endl;
     return 0;
 }
